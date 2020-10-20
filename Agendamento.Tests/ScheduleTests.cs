892cefd739d39c82a31c578b351f4c1046d88968
Diego Aquino <diegol.aquino@gmail.com>
2020-10-19 21:05:51 -0300

    inserindo testes na api.

---
+++ Agendamento.Tests/ScheduleTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agendamento.Tests
+{
+    [TestClass]
+    public class ScheduleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+    }
+}
