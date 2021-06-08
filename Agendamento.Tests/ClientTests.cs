316bd07d6be1893e873c381cd9661aeccbc61b61
Diego Aquino <diegol.aquino@gmail.com>
2021-06-07 21:09:57 -0300

    Adicionando classes de teste

---
+++ Agendamento.Tests/ClientTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agendamento.Tests
+{
+    [TestClass]
+    public class ClientTests
+    {
+    }
+}
